eb412833d548483c076903f6e6fdfe91af78fd07
wildemberg-sales <junior_sales2010@hotmail.com>
2024-01-10 12:37:08 -0300

    criado função de busca de produtos

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,21 +8,21 @@ import React from 'react'
 
 
 export default function Home() {
-const [data, setdata]=React.useState();
+const [data, setData]=React.useState([]);
 
-async function getProdutos(){
-  await axios.get('http://127.0.0.1:8000/produtos/')
+async function getAllProdutos(){
+  await axios.get('https://back-end-ecommerce-xi7j.onrender.com/produtos/')
   .then((res)=>{
-    // console.log(res.data);
-    setdata(res.data);
+    // salva os dados na variavel data
+    setData(res.data);
+    console.log(data)
   }).catch((error)=>console.log(error));
-  console.log(data);
 };
 
   return(
     <div>
-  <Header></Header>
-  <button onClick={async ()=>{await this.getProdutos()}}> clique aqui </button>
+      <Header></Header>
+      <button onClick={getAllProdutos}> clique aqui </button>
     </div>
     )
 }
