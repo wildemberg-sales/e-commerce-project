6123c486344373b5c3ff72a5aaf4f9115aee9548
Caiomesvie <caioltwin@gmail.com>
2024-01-09 23:08:49 -0300

    testando requisicoes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,28 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from './header/Header'
+import axios from 'axios'
+import React from 'react'
+// import {useState} from 'react'
+
 
 export default function Home() {
-return(
+const [data, setdata]=React.useState();
+
+async function getProdutos(){
+  await axios.get('http://127.0.0.1:8000/produtos/')
+  .then((res)=>{
+    // console.log(res.data);
+    setdata(res.data);
+  }).catch((error)=>console.log(error));
+  console.log(data);
+};
+
+  return(
+    <div>
   <Header></Header>
-)
+  <button onClick={async ()=>{await this.getProdutos()}}> clique aqui </button>
+    </div>
+    )
 }
