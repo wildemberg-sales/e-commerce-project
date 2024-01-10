55a8add9c1ea78e0eb3d0a610846fd3b5688baa6
wildemberg-sales <junior_sales2010@hotmail.com>
2024-01-10 12:30:43 -0300

    configurado acesso de todas as rotas

--- backEnd/backEnd/settings.py
+++ backEnd/backEnd/settings.py
@@ -55,9 +55,11 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware'
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/'
-]
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backEnd.urls'
 
