486f359fd8200d0016dca08fe9062533e7779a2e
wildemberg-sales <junior_sales2010@hotmail.com>
2024-01-22 13:17:36 -0300

    Ajustando pastas do front e back

--- backEnd/produtos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Produtos
-admin.site.register(Produtos)
--- backEnd/produtos/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Produtos
-
-# Cria modelo para ser retornado como json na view
-class ProdutosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Produtos
-        fields = '__all__'
